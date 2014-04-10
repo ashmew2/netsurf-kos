0ae62e09f22b1ea21428315cec76008ad3ec015d
ashmew2 <money_man_91@yahoo.com>
2014-04-10 20:41:50 +0530

    ?

--- netsurf/netsurf/content/llcache.c
+++ netsurf/netsurf/content/llcache.c
@@ -1623,6 +1623,7 @@ static void llcache_fetch_callback(const fetch_msg *msg, void *p)
 	nserror error = NSERROR_OK;
 	llcache_object *object = p;
 	llcache_event event;
+	__menuet__debug_out("Inside llcahce_fetch_callback\n");
 
 #ifdef LLCACHE_TRACE
 	LOG(("Fetch event %d for %p", msg->type, object));
