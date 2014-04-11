357846dd7043d2d0c197d0ae400614425ddbdb47
ashmew2 <money_man_91@yahoo.com>
2014-04-11 17:51:35 +0530

    Synced with Rev 4821 on KolibriOS' SVN

--- netsurf/libercurl/curl.c
+++ netsurf/libercurl/curl.c
@@ -3,32 +3,32 @@
 
 char *curl_unescape( char * url , int length ) {
 	
-	__menuet__debug_out("CURL:UNESCAPE\n");
+	__menuet__debug_out("CURL:unescape\n");
 	return url;
 	
 }
 
 void curl_free( char * ptr ){
-	__menuet__debug_out("CURL:FREE?????\n");
+	__menuet__debug_out("CURL:free?\n");
 }
 
 char *curl_easy_unescape( CURL * curl , char * url , int inlength , int * outlength ){
-	__menuet__debug_out("CURL:EASYUNESCAPE!\n");
+	__menuet__debug_out("CURL:easyunescape!\n");
 	*outlength=inlength;
 	return url;
 	
 }
 
 void curl_easy_cleanup(CURL * handle ) {
-	__menuet__debug_out("CURL:CLEANUP!!\n");
+	__menuet__debug_out("CURL:cleanup\n");
 }
 
 CURL *curl_easy_init( ){
-	__menuet__debug_out("CURL:INIT\n");
+	__menuet__debug_out("CURL:init\n");
 }
 
 time_t curl_getdate(char * datestring , time_t *now ){
-	__menuet__debug_out("CURL:GETDATE\n");
+	__menuet__debug_out("CURL:getdate\n");
 	return 0;
 	
 }
