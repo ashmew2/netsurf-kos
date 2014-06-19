5361030d574778f77959b3eea3203ad001f72a06
ashmew2 <money_man_91@yahoo.com>
2014-06-19 14:27:24 +0530

    Fixed macro in stubs.c

--- netsurf/netsurf/objs/stubs.c
+++ netsurf/netsurf/objs/stubs.c
@@ -5,8 +5,8 @@
 #ifdef DBG
 #undef DBG
 #endif
-//#define DBG(s) __menuet__debug_out(s) /* For the debug messages in BOARD */
-#define DBG(s) LOG((s))            /* So that we see debug in Netsurf's LOG files */
+#define DBG(s) __menuet__debug_out(s) /* For the debug messages in BOARD */
+//#define DBG(s) LOG((s))            /* So that we see debug in Netsurf's LOG files */
 
 /*	
 long int  __divdi3(long int  a, long int  b) {
