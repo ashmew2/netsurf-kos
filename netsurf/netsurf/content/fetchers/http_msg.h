73739a6806d805ca29d71ff7020dc2be7abd2207
ashmew2 <money_man_91@yahoo.com>
2014-07-25 00:07:39 +0530

    Removed flags for now

--- netsurf/netsurf/content/fetchers/http_msg.h
+++ netsurf/netsurf/content/fetchers/http_msg.h
@@ -18,20 +18,4 @@ unsigned int content_length;
 unsigned int content_received;
 char header; //unknown size (actually, it's size is defined in header_length)
 };
-
-/* For the flags in the http_msg struct above  */
-int FLAG_HTTP11             = 1 << 0;
-int FLAG_GOT_HEADER         = 1 << 1;
-int FLAG_GOT_ALL_DATA       = 1 << 2;
-int FLAG_CONTENT_LENGTH     = 1 << 3;
-int FLAG_CHUNKED            = 1 << 4;
-int FLAG_CONNECTED          = 1 << 5;
-
-/* ERROR flags go into the upper word */
-int FLAG_INVALID_HEADER     = 1 << 16;
-int FLAG_NO_RAM             = 1 << 17;
-int FLAG_SOCKET_ERROR       = 1 << 18;
-int FLAG_TIMEOUT_ERROR      = 1 << 19;
-int FLAG_TRANSFER_FAILED    = 1 << 20;
-
 #endif
